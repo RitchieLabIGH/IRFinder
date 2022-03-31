3081564744ce2164ba374a60f623e1ae535954cb
cloxd <claudio.lorenzi@gmail.com>
2022-03-31 14:16:36 +0200

    Added output as requested in issue 10

--- bin/util/deseq2.R
+++ bin/util/deseq2.R
@@ -11,6 +11,7 @@ source(other.name)
 # source("~/git/IRFinder/bin/DESeq2Constructor.R")
 ### Read args
 # setwd("~/test/IRFinder2/Diff/sing/")
+# setwd("/media/lorencl/f4e6cecd-2fb8-4aa6-991c-620f450fd511/works/IRFinder2/Diff/test_9")
 # args=c("./groups.tsv", "0.05", "0" , "0" ,"0")
 
 args <- commandArgs(trailingOnly = T)
@@ -45,12 +46,28 @@ design(dds) = ~Condition + Condition:IRFinder
 conditions=levels(experiment$Condition)
 dds = DESeq(dds)
 resultsNames(dds)
+nn_counts = counts(dds, normalized=F)
+global_dat = data.frame( intron = rownames(nn_counts) );
+for ( i in 1:nrow(experiment) ) {
+  s_name =experiment$SampleName[i]
+	global_dat[,paste0("IRratio.",s_name)] = nn_counts[, paste0("intronDepth.", s_name)] / (nn_counts[, paste0("intronDepth.", s_name)] + nn_counts[, paste0("maxSplice.", s_name)] )
+	global_dat[,paste0("IRratio.",s_name)][is.na(global_dat[,paste0("IRratio.",s_name)])]=0
+}
+
+for ( i in 1:(length(conditions)) ){
+	global_dat[,paste0(conditions[i], ".Mean.IRratio")]= rowMeans(global_dat[,paste0("IRratio.",experiment$SampleName[experiment$Condition == conditions[i]])])
+	
+}
+
 
 for ( i in 1:(length(conditions)-1) ){
   for (j in (i+1):length(conditions)){
     contrast_name=paste0(conditions[i], "_", conditions[j])
     res = results(dds, contrast=list(paste0("Condition", conditions[i] ,".IRFinderIR"),paste0("Condition", conditions[j] ,".IRFinderIR")), cooksCutoff=cooks_cutoff, independentFiltering=independentFiltering)  
     res$padj[is.na(res$padj)]=1
+    global_dat[,paste0("DESeq2.padj.", contrast_name)]=res$padj
+    global_dat[,paste0("DESeq2.baseMean.", contrast_name)]=res$baseMean
+    global_dat[,paste0("DESeq2.log2FoldChange.", contrast_name)]=res$log2FoldChange
     if ( sum(res$padj < 0.05 ) > 0 ){
       pdf(paste0(out_folder, "/", contrast_name, "_plot.pdf"))
       nn_counts = counts(dds, normalized=F)
@@ -66,6 +83,9 @@ for ( i in 1:(length(conditions)-1) ){
     write.table(res, file = paste0(out_folder, "/", contrast_name, "_DESeq2.tsv") ,sep="\t", quote = F)
   }
 }
+rownames(global_dat)=global_dat$intron
+global_dat=global_dat[,-1]
+write.table(global_dat, file = paste0(out_folder, "/all_results_DESeq2.tsv") ,sep="\t", quote = F)
 
 quit(save = "no")
 
